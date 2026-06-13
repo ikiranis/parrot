30aece517f289c945db94ef5fe9a3256f167b8fc
Yiannis Kiranis <rocean74@gmail.com>
2026-06-13 17:47:25 +0300

    feat: Remove delete button and associated styles from PhotoDetail.vue

--- frontend/src/components/PhotoDetail.vue
+++ frontend/src/components/PhotoDetail.vue
@@ -161,20 +161,6 @@ watch(
 			</svg>
 		</button>
 
-		<!-- Delete button -->
-		<button
-			v-if="photo"
-			class="viewer-btn viewer-delete"
-			@click="showDeleteModal = true"
-			:aria-label="language.get('Delete')"
-			:title="language.get('Delete') + ' (D)'"
-		>
-			<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-				<path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
-				<path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
-			</svg>
-		</button>
-
 		<!-- Loading -->
 		<div v-if="loading" class="viewer-loading">
 			<div class="spinner-border text-light" role="status">
@@ -378,16 +364,6 @@ watch(
 	.viewer-close {
 		top: 12px;
 		left: 12px;
-	}
-
-	.viewer-delete {
-		top: 12px;
-		left: 52px;
-	}
-
-	.viewer-delete:hover {
-		background: rgba(192, 57, 43, 0.75);
-		border-color: rgba(231, 76, 60, 0.5);
 	}
 
 	.nav-btn {
